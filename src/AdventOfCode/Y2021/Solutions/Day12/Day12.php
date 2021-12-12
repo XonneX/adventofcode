6f78efb493c563456c0a40a42347db6ba3d770b5
Janik Baumann <XonneX@users.noreply.github.com>
2021-12-12 18:45:38 +0100

    Day 12 2021

--- src/AdventOfCode/Y2021/Solutions/Day12/Day12.php
+++ src/AdventOfCode/Y2021/Solutions/Day12/Day12.php
@@ -7,20 +7,134 @@ namespace XonneX\AdventOfCode\Y2021\Solutions\Day12;
 use RuntimeException;
 use XonneX\AdventOfCode\Core\AbstractSolution;
 
+use function array_count_values;
+use function array_search;
+use function array_unique;
+use function array_values;
+use function count;
+use function ctype_lower;
+use function ctype_upper;
+use function explode;
+use function in_array;
+use function print_r;
+use function rsort;
+use function sort;
+use function sprintf;
+use function str_contains;
+
+use function str_split;
+use function var_dump;
+
+use const PHP_EOL;
+
 class Day12 extends AbstractSolution
 {
     public function __construct()
     {
         parent::__construct(2021, 12);
-    } 
+    }
 
     protected function partOne(string $input): string
     {
-        throw new RuntimeException('Not implemented yet');
+        $map = [];
+        foreach (explode("\n", $input) as $line) {
+            [$start, $adjacent] = explode('-', $line);
+
+            if ($start !== 'end' && $adjacent !== 'start') {
+                $map[$start][] = $adjacent;
+            }
+
+            if ($start !== 'start' && $adjacent !== 'end') {
+                $map[$adjacent][] = $start;
+            }
+        }
+        $paths = $this->generatePaths('start', $map['start'], $map);
+
+        return (string) count($paths);
     }
 
     protected function partTwo(string $input): string
     {
-        throw new RuntimeException('Not implemented yet');
+        $map = [];
+        foreach (explode("\n", $input) as $line) {
+            [$start, $adjacent] = explode('-', $line);
+
+            if ($start !== 'end' && $adjacent !== 'start') {
+                $map[$start][] = $adjacent;
+            }
+
+            if ($start !== 'start' && $adjacent !== 'end') {
+                $map[$adjacent][] = $start;
+            }
+        }
+
+        $paths = $this->generatePathsTwo('start', $map['start'], $map);
+
+        return (string) count($paths);
+    }
+
+    private function generatePaths(string $path, array $siblings, array $map): array
+    {
+        $paths = [];
+
+        foreach ($siblings as $sibling) {
+            $newPath = $path . ',' . $sibling;
+
+            if ($sibling === 'end') {
+                $paths[] = $newPath;
+                continue;
+            }
+
+            if (ctype_lower($sibling) && str_contains($path, ',' . $sibling)) {
+                continue;
+            }
+
+            foreach ($this->generatePaths($newPath, $map[$sibling], $map) as $value) {
+                $paths[] = $value;
+            }
+        }
+
+        return $paths;
+    }
+
+    private function generatePathsTwo(string $path, array $siblings, array $map): array
+    {
+        $paths = [];
+
+        foreach ($siblings as $sibling) {
+            $newPath = $path . ',' . $sibling;
+
+            if ($sibling === 'end') {
+                $paths[] = $newPath;
+                continue;
+            }
+
+            if (ctype_lower($sibling) && (str_contains($path, ',' . $sibling) && !$this->checkSmallCaveAgain($path))) {
+                continue;
+            }
+
+            foreach ($this->generatePathsTwo($newPath, $map[$sibling], $map) as $value) {
+                $paths[] = $value;
+            }
+        }
+
+        return $paths;
+    }
+
+    private function checkSmallCaveAgain(string $path): bool
+    {
+        $counts = array_count_values(explode(',', $path));
+
+        foreach ($counts as $char => $count) {
+            if ($char === 'start' || ctype_upper($char)) {
+                continue;
+            }
+
+            if ($count > 1) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- tests/AdventOfCode/Y2021/Solutions/Day12/Day12Test.php
+++ tests/AdventOfCode/Y2021/Solutions/Day12/Day12Test.php
@@ -13,18 +13,35 @@ class Day12Test extends TestCase
         $day12 = new Day12();
 
         $day12->setDebugInput(<<<TXT
-NO_EXAMPLE_INITIALIZED
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
 TXT
         );
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day12->solvePartOne());
+        self::assertSame('226', $day12->solvePartOne());
     }
 
     public function testSolvePartOne(): void
     {
         $day12 = new Day12();
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day12->solvePartOne());
+        self::assertSame('5254', $day12->solvePartOne());
     }
 
     public function testSolvePartTwoExample(): void
@@ -32,17 +49,34 @@ TXT
         $day12 = new Day12();
 
         $day12->setDebugInput(<<<TXT
-NO_EXAMPLE_INITIALIZED
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
 TXT
         );
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day12->solvePartTwo());
+        self::assertSame('3509', $day12->solvePartTwo());
     }
 
     public function testSolvePartTwo(): void
     {
         $day12 = new Day12();
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day12->solvePartTwo());
+        self::assertSame('149385', $day12->solvePartTwo());
     }
 }
