8c50b1912c9b23fec1460d570154646467090de5
Janik Baumann <janik.baumann@bertschi.com>
2022-12-04 20:36:21 +0100

    Day 13 2021

--- src/AdventOfCode/Y2021/Solutions/Day13/Day13.php
+++ src/AdventOfCode/Y2021/Solutions/Day13/Day13.php
@@ -7,20 +7,235 @@ namespace XonneX\AdventOfCode\Y2021\Solutions\Day13;
 use RuntimeException;
 use XonneX\AdventOfCode\Core\AbstractSolution;
 
+use function array_key_exists;
+use function array_keys;
+use function array_reverse;
+use function explode;
+use function ksort;
+use function max;
+use function print_r;
+use function range;
+use function substr;
+
+use function var_dump;
+
+use const PHP_EOL;
+
 class Day13 extends AbstractSolution
 {
     public function __construct()
     {
         parent::__construct(2021, 13);
-    } 
+    }
 
     protected function partOne(string $input): string
     {
-        throw new RuntimeException('Not implemented yet');
+        [$dotsLines, $foldsLines] = explode("\n\n", $input);
+
+        $maxX = 0;
+        $maxY = 0;
+        $grid = [];
+
+        foreach (explode("\n", $dotsLines) as $line) {
+            [$x, $y] = explode(',', $line);
+
+            $grid[$y][$x] = true;
+
+            if ($x > $maxX) {
+                $maxX = $x;
+            }
+
+            if ($y > $maxY) {
+                $maxY = $y;
+            }
+        }
+
+        $tmpY = $maxY;
+        while ($tmpY >= 0) {
+            $tmpX = $maxX;
+
+            while ($tmpX >= 0) {
+                $foldValue = $grid[$tmpY][$tmpX] ?? null;
+
+                if ($foldValue === null) {
+                    $grid[$tmpY][$tmpX] = false;
+                }
+
+                $tmpX--;
+            }
+
+            ksort($grid[$tmpY]);
+
+            $tmpY--;
+        }
+        ksort($grid);
+
+        foreach (explode("\n", $foldsLines) as $foldLine) {
+            $foldLine = substr($foldLine, 11);
+            [$direction, $foldValue] = explode('=', $foldLine);
+
+            $foldValue = (int) $foldValue;
+
+            $tmpGrid = [];
+
+            if ($direction === 'y') {
+                foreach (range($foldValue + 1, $maxY) as $y) {
+                    $tmpGrid[$y] = $grid[$y];
+                    unset($grid[$y]);
+                }
+
+                unset($grid[$foldValue]);
+
+                $tmpGrid = array_reverse($tmpGrid);
+            } else {
+                foreach ($grid as $y => $row) {
+                    foreach (range($foldValue, $maxX) as $x) {
+                        $value = $row[$x];
+
+                        $tmpGrid[$y][$x] = $value;
+
+                        unset($grid[$y][$x]);
+                    }
+
+                    $tmpGrid[$y] = array_reverse($tmpGrid[$y]);
+                }
+            }
+
+            foreach ($tmpGrid as $y => $row) {
+                foreach ($row as $x => $value) {
+                    if (!$value) {
+                        continue;
+                    }
+
+                    $grid[$y][$x] = $value;
+                }
+            }
+
+            break;
+        }
+
+        $counter = 0;
+
+        foreach ($grid as $row) {
+            foreach ($row as $foldValue) {
+                if ($foldValue) {
+                    $counter++;
+                }
+            }
+        }
+
+        return (string) $counter;
     }
 
     protected function partTwo(string $input): string
     {
-        throw new RuntimeException('Not implemented yet');
+        [$dotsLines, $foldsLines] = explode("\n\n", $input);
+
+        $maxX = 0;
+        $maxY = 0;
+        $grid = [];
+
+        foreach (explode("\n", $dotsLines) as $line) {
+            [$x, $y] = explode(',', $line);
+
+            $grid[$y][$x] = true;
+
+            if ($x > $maxX) {
+                $maxX = $x;
+            }
+
+            if ($y > $maxY) {
+                $maxY = $y;
+            }
+        }
+
+        $tmpY = $maxY;
+        while ($tmpY >= 0) {
+            $tmpX = $maxX;
+
+            while ($tmpX >= 0) {
+                $foldValue = $grid[$tmpY][$tmpX] ?? null;
+
+                if ($foldValue === null) {
+                    $grid[$tmpY][$tmpX] = false;
+                }
+
+                $tmpX--;
+            }
+
+            ksort($grid[$tmpY]);
+
+            $tmpY--;
+        }
+        ksort($grid);
+
+        foreach (explode("\n", $foldsLines) as $foldLine) {
+            $foldLine = substr($foldLine, 11);
+            [$direction, $foldValue] = explode('=', $foldLine);
+
+            $foldValue = (int) $foldValue;
+
+            $tmpGrid = [];
+
+            if ($direction === 'y') {
+                foreach (range($foldValue + 1, $maxY) as $y) {
+                    $tmpGrid[$y] = $grid[$y];
+                    unset($grid[$y]);
+                }
+
+                unset($grid[$foldValue]);
+
+                $maxY = max(array_keys($grid));
+
+                $tmpGrid = array_reverse($tmpGrid);
+            } else {
+                foreach ($grid as $y => $row) {
+                    foreach (range($foldValue + 1, $maxX) as $x) {
+                        $value = $row[$x];
+
+                        $tmpGrid[$y][$x] = $value;
+
+                        unset($grid[$y][$x]);
+                    }
+
+                    unset($grid[$y][$foldValue]);
+
+                    $tmpGrid[$y] = array_reverse($tmpGrid[$y]);
+                }
+
+                $maxX = max(array_keys($grid[$y]));
+            }
+
+            foreach ($tmpGrid as $y => $row) {
+                foreach ($row as $x => $value) {
+                    if (!$value) {
+                        continue;
+                    }
+
+                    $grid[$y][$x] = $value;
+                }
+            }
+        }
+
+        return $this->dumpGrid($grid);
+    }
+
+    private function dumpGrid(array $grid): string
+    {
+        $res = PHP_EOL;
+
+        foreach ($grid as $row) {
+            foreach ($row as $value) {
+                if ($value) {
+                    $res .= '#';
+                } else {
+                    $res .= '.';
+                }
+            }
+
+            $res .= PHP_EOL;
+        }
+
+        return $res;
     }
 }
--- tests/AdventOfCode/Y2021/Solutions/Day13/Day13Test.php
+++ tests/AdventOfCode/Y2021/Solutions/Day13/Day13Test.php
@@ -13,18 +13,45 @@ class Day13Test extends TestCase
         $day13 = new Day13();
 
         $day13->setDebugInput(<<<TXT
-NO_EXAMPLE_INITIALIZED
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5
 TXT
         );
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day13->solvePartOne());
+        self::assertSame('17', $day13->solvePartOne());
+    }
+
+    public function testSolvePartOneTooHigh(): void
+    {
+        $day13 = new Day13();
+
+        self::assertLessThan('815', $day13->solvePartOne());
     }
 
     public function testSolvePartOne(): void
     {
         $day13 = new Day13();
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day13->solvePartOne());
+        self::assertSame('693', $day13->solvePartOne());
     }
 
     public function testSolvePartTwoExample(): void
@@ -32,11 +59,99 @@ TXT
         $day13 = new Day13();
 
         $day13->setDebugInput(<<<TXT
-NO_EXAMPLE_INITIALIZED
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5
+fold along x=2
+fold along y=3
 TXT
         );
 
-        self::assertSame('NO_SOLUTION_INITIALIZED', $day13->solvePartTwo());
+        $expected = '
+#####
+#...#
+#...#
+#...#
+#####
+.....
+.....
+';
+
+        self::assertSame($expected, $day13->solvePartTwo());
+    }
+
+    public function testSolvePartTwoExampleTwo(): void
+    {
+        $day13 = new Day13();
+
+        $day13->setDebugInput(<<<TXT
+1049,1721
+2686,1404
+3385,2284
+1380,2354
+3134,2238
+1413,563
+2053,1247
+2430,163
+2334,1208
+1221,798
+444,1884
+2656,2241
+798,1473
+3873,2314
+318,1952
+537,1032
+2848,2551
+3461,313
+2053,248
+3555,524
+761,76
+446,1596
+
+fold along y=1279
+fold along y=639
+fold along y=319
+fold along x=2047
+fold along x=1023
+fold along x=511
+fold along x=255
+fold along x=127
+fold along y=159
+fold along y=79
+fold along x=63
+fold along y=39
+fold along x=31
+fold along x=15
+fold along y=19
+TXT
+        );
+
+        self::assertSame('HELP', $day13->solvePartTwo());
+    }
+
+    public function testSolvePartTwoNot(): void
+    {
+        $day13 = new Day13();
+
+        self::assertNotSame('UGLORAPU', $day13->solvePartTwo());
     }
 
     public function testSolvePartTwo(): void
